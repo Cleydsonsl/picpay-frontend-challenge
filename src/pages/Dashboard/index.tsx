c91974446a12896a380c4b9aeb4527aad83b6c8a
Cleydson Souza <Cleydson.souza@jsafrasarasin.com>
2022-10-23 11:23:39 -0300

    feat: building screen body

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 
-import { Container } from './styles';
+import { Body, Container, Content, Title } from './styles';
 
 export function Dashboard(){
   return (
     <Container>
       <Header/>
+      <Body>
+        <Content>
+          <Title>Meus pagamentos</Title>
+          <Button className='addButton' title='ADICIONAR PAGAMENTO' />
+        </Content>
+      </Body>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
+import { mixins } from '../../styles/mixins';
 
 export const Container = styled.div`
   flex: 1;
+`;
+
+export const Body = styled.div`
+  padding: 80px;
+`;
+
+export const Content = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .addButton {
+    max-width: 333px;
+    font-weight: 500;
+    background: ${mixins.colors.secondary};
+  }
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+  font-size: 36px;
+  color: ${mixins.colors.text};
 `;
