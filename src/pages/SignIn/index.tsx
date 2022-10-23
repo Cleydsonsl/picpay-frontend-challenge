9af89a63e08bfbfc746705cdb6244733d6ce66ae
Cleydson Souza <Cleydson.souza@jsafrasarasin.com>
2022-10-23 00:40:30 -0300

    feat: created styles signin screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-import { Container } from './styles';
+import brandImg from '../../assets/brand.svg'
+
+import { Container, Brand, Login } from './styles';
 
 export function SignIn(){
   return (
     <Container>
+      <Login/>
+      <Brand>
+        <img src={brandImg} alt="brand" />
+      </Brand>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
+import { mixins } from '../../styles/mixins';
 
 export const Container = styled.div`
+  display: flex;
   flex: 1;
+  background: ${mixins.colors.background};
 `;
+
+export const Login = styled.div`
+  width: 33%;
+`;
+
+export const Brand = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: ${mixins.colors.shape};
+
+  img {
+    max-height: 943px;
+  }
+`
