b0ee889ca253a468ec4a48f4116eb5c471c7f697
Cleydson Souza <Cleydson.souza@jsafrasarasin.com>
2022-10-23 00:19:43 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+import { mixins } from './mixins';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-size: 60%
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html { 
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+    
+    body { 
+        background: ${mixins.colors.background};
+        -webkit-font-smoothing: antialiased;
+    }
+    button { 
+        cursor: pointer;
+    }
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
---
+++ src/styles/mixins.ts
@@ -0,0 +1,24 @@
+export const mixins = {
+  colors: {
+    background: '#EDEDED',
+
+    primary: '#153250',
+    secondary: '#8CB1F5',
+
+    red: '#F34040',
+    border: '#D9D9D9',
+    shape: '#FFFFFF',
+
+    text: '#4E4E4E',
+    text_light: '#D1D1D1',
+
+    error: '#CE2020'
+
+  },
+  fonts: {
+    abel: 'Abel-Regular',
+    light: 'Lato-Light',
+    regular: 'Lato-Regular',
+    bold: 'Lato-Bold'
+  }
+}
